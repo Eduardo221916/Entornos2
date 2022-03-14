8bf932dfef5ef54f1218b0c5be59758bd25dbe45
LENOVO <LENOVO@LAPTOP-99UM7LE1>
2022-03-14 17:26:54 +0100

    mensaje de confirmacion

--- mensaje/Mensaje.java
+++ mensaje/Mensaje.java
@@ -1,8 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-package mensaje;
 
 /**
  *
@@ -13,8 +12,7 @@ public class Mensaje {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("hola");
+    public static void main(String args[]) {
+        System.out.println("Hola");
     }
-    
 }
